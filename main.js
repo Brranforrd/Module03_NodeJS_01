558e433f5eb88650c33cfe06ba1b3acbbd1b30f9
Branford Brown <bjulianbrown@gmail.com>
2025-10-21 11:45:49 -0500

    Create

---
+++ main.js
@@ -0,0 +1,16 @@
+ javascript
+      // Operations: Object Access, Invocation
+      console.log("What's your name?");
+
+      // Operations:
+      process.stdin.on('data', function(data) {
+
+          // Operations:
+          const name = data.toString().trim();
+          
+          // Operations:
+          console.log(`Hello, ${name}!`);
+
+      });
+      
+      
